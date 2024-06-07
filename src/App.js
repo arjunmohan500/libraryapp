3bf387a86478783605f37dca20d983cba74bbfbc
arjunmohan500 <arjunmohan500@gmail.com>
2024-06-07 14:24:49 +0530

    created

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import AddLibrary from './components/AddLibrary';
 import SearchStudent from './components/SearchStudent';
 import DeleteBook from './components/DeleteBook';
 import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddLibrary/>
-      <SearchStudent/>
-      <DeleteBook/>
-      <ViewAll/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+<Route path='/' element={<AddLibrary/>}/>
+<Route path='/SearchStudent' element={<SearchStudent/>}/>
+<Route path='/DeleteBook' element={<DeleteBook/>}/>
+<Route path='/ViewAll' element={<ViewAll/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
--- src/components/AddLibrary.jsx
+++ src/components/AddLibrary.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const AddLibrary = () => {
     return (
         <div>
+            <NavBar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
